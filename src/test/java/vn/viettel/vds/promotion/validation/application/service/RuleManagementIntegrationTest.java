675d30b9869c57a2e704dbda3021744392806838
harley hoang <harley.hoang.work@gmail.com>
2026-04-25 16:18:46 +0700

    fix(validation): remove RuleLinter from RuleManagementService constructor calls in tests (Task 09 V7)

--- src/test/java/vn/viettel/vds/promotion/validation/application/service/RuleManagementIntegrationTest.java
+++ src/test/java/vn/viettel/vds/promotion/validation/application/service/RuleManagementIntegrationTest.java
@@ -15,7 +15,6 @@ import vn.viettel.vds.promotion.validation.application.port.out.OperatorPersiste
 import vn.viettel.vds.promotion.validation.application.port.out.RuleBindingPersistencePort;
 import vn.viettel.vds.promotion.validation.application.port.out.RuleEngineClient;
 import vn.viettel.vds.promotion.validation.application.port.out.RulePersistencePort;
-import vn.viettel.vds.promotion.validation.application.service.RuleLinter;
 import vn.viettel.vds.promotion.validation.domain.model.Operator;
 import vn.viettel.vds.promotion.validation.domain.model.Rule;
 import vn.viettel.vds.promotion.validation.domain.model.RuleBinding;
@@ -71,7 +70,7 @@ class RuleManagementIntegrationTest {
                 .thenReturn(List.of());
 
         ruleManagementService = new RuleManagementService(
-                rulePort, assembler, validator, new RuleLinter(), dslGenerator, drlCompiler,
+                rulePort, assembler, validator, dslGenerator, drlCompiler,
                 ruleEngineClient, operatorPort, Optional.empty());
         bindingManagementService = new RuleBindingManagementService(bindingPort);
     }
--- src/test/java/vn/viettel/vds/promotion/validation/application/service/RuleManagementServiceHistoryTest.java
+++ src/test/java/vn/viettel/vds/promotion/validation/application/service/RuleManagementServiceHistoryTest.java
@@ -15,7 +15,6 @@ import vn.viettel.vds.promotion.validation.application.port.out.OperatorPersiste
 import vn.viettel.vds.promotion.validation.application.port.out.RuleEngineClient;
 import vn.viettel.vds.promotion.validation.application.port.out.RuleHistoryPersistencePort;
 import vn.viettel.vds.promotion.validation.application.port.out.RulePersistencePort;
-import vn.viettel.vds.promotion.validation.application.service.RuleLinter;
 import vn.viettel.vds.promotion.validation.domain.model.Rule;
 import vn.viettel.vds.promotion.validation.domain.model.RuleHistoryEntry;
 
@@ -63,7 +62,7 @@ class RuleManagementServiceHistoryTest {
         DrlCompiler drlCompiler = new DrlCompiler();
 
         service = new RuleManagementService(
-                rulePort, assembler, validator, new RuleLinter(), dslGenerator, drlCompiler,
+                rulePort, assembler, validator, dslGenerator, drlCompiler,
                 ruleEngineClient, operatorPort, Optional.of(historyPort));
 
         when(rulePort.save(any(Rule.class))).thenAnswer(inv -> inv.getArgument(0));
@@ -253,7 +252,7 @@ class RuleManagementServiceHistoryTest {
         ObjectMapper om = new ObjectMapper();
         RuleManagementService serviceNoHistory = new RuleManagementService(
                 rulePort, new RuleTreeAssembler(), new RuleValidator(om),
-                new RuleLinter(), new DslGenerator(om), new DrlCompiler(),
+                new DslGenerator(om), new DrlCompiler(),
                 ruleEngineClient, operatorPort, Optional.empty()
         );
 
